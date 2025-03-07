c2b3871b431f520a154733e9f67a25e34dd97799
Anchana <fatimarajananchana@gmail.com>
2025-03-07 13:40:14 +0100

    api symphony

---
+++ API/API/src/Repository/UserRepository.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<User>
+ */
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+      // Méthode pour récupérer tous les utilisateurs
+      public function findAllUsers(): array
+      {
+          return $this->findAll();
+      }
+
+       // Méthode pour insérer un utilisateur
+    public function saveUser(User $user): void
+    {
+        $entityManager = $this->getEntityManager();
+        $entityManager->persist($user); // Prépare l'utilisateur pour insertion
+        $entityManager->flush();       // Enregistre les modifications dans la base
+    }
+    
+    //    /**
+    //     * @return User[] Returns an array of User objects
+    //     */
+    //    public function findByExampleField($value): array
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->orderBy('u.id', 'ASC')
+    //            ->setMaxResults(10)
+    //            ->getQuery()
+    //            ->getResult()
+    //        ;
+    //    }
+
+    //    public function findOneBySomeField($value): ?User
+    //    {
+    //        return $this->createQueryBuilder('u')
+    //            ->andWhere('u.exampleField = :val')
+    //            ->setParameter('val', $value)
+    //            ->getQuery()
+    //            ->getOneOrNullResult()
+    //        ;
+    //    }
+}
