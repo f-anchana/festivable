ad97ed91523dac41c9b74095a26ad1dafef19dc0
Loana chalach <101940424+Loaxn@users.noreply.github.com>
2025-05-15 23:07:42 +0200

    PARTIE ENGAGEMENT DONE

--- front/src/components/FestivalCard/FestivalCard.jsx
+++ front/src/components/FestivalCard/FestivalCard.jsx
@@ -2,13 +2,23 @@ import React from 'react';
 import Image from 'next/image';
 import styles from './FestivalCard.module.css';
 
-export default function FestivalCard({ title, date, price, image }) {
+export default function FestivalCard({ title, organisation, date, price, image }) {
   return (
     <div className={styles.card}>
-      <Image src={image} alt={title} width={400} height={200} className={styles.image} />
-      <h4 className={styles.title}>{title}</h4>
-      <p className={styles.date}>{date}</p>
-      <p className={styles.price}>{price}</p>
+      <div className={styles.imageWrapper}>
+        <Image
+          src={image}
+          alt={title}
+          fill
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.info}>
+        <h4 className={styles.title}>{title}</h4>
+        <p className={styles.organisation}>{organisation}</p>
+        <p className={styles.date}>{date}</p>
+        <p className={styles.price}>{price}</p>
+      </div>
     </div>
   );
 }
