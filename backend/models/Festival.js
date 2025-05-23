f39b2b0f6d33d555a6e79763676f7601808d3a3f
Alina <108938418+Aliloa@users.noreply.github.com>
2025-05-23 14:27:12 +0200

    API festivals

---
+++ backend/models/Festival.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const PricesSchema = new mongoose.Schema({
+    type: String,
+    amount: Number
+});
+
+const RecruitmentSchema = new mongoose.Schema({
+    position: String,
+    start_date: Date,
+    end_date: Date,
+    paid: Boolean,
+    contact_email: String
+});
+
+const AccessibilitySchema = new mongoose.Schema({
+    wheelchair_accessible: Boolean,
+    disabled_parking_available: Boolean
+    // Ajouter d'autres champs ici
+});
+
+const FestivalSchema = new mongoose.Schema({
+    title: String,
+    start_date: Date,
+    end_date: Date,
+    address: String,
+    description: String,
+    link: String,
+    prices: [PricesSchema],
+    recruitments: [RecruitmentSchema],
+    accessibility: AccessibilitySchema
+});
+
+// Ajouter images
+
+// Ajouter map
+
+module.exports = mongoose.model('Festival', FestivalSchema, 'festivals');
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const festivalController = require('../controllers/festivalController')
 
 router.get('/users', userController.getUsers);
 router.post('/user', userController.createUser);
+router.get('/festivals', festivalController.getFestivals);
+router.post('/festival', festivalController.createFestival);
 
 module.exports = router;
