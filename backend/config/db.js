4cbf5e78f609a4882ebb8b473f7e319ac9b52357
Anchana <fatimarajananchana@gmail.com>
2025-06-06 11:12:52 +0200

    update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 //ici c'est le fichier qui sert la connection à la bdd
 
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+require('dotenv').config();
 
 const connectDB = async () => {
     mongoose.connect(process.env.MONGO_URI)
