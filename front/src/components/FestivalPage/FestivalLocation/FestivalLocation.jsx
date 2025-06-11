0641ba93ac524526bf104b3dbd64420516d60816
Loana chalach <101940424+Loaxn@users.noreply.github.com>
2025-06-11 11:30:28 +0200

    carte afficher pour chaque festival

--- front/src/components/FestivalPage/FestivalLocation/FestivalLocation.jsx
+++ front/src/components/FestivalPage/FestivalLocation/FestivalLocation.jsx
@@ -1,15 +1,29 @@
+
 'use client';
 
 import s from './FestivalLocation.module.scss';
 
-export default function FestivalLocation({ location }) {
-  if (!location) return <p>Localisation non disponible.</p>;
+export default function FestivalLocation({ id }) {
+  const embedUrl = `${process.env.NEXT_PUBLIC_API_URL}/embed-map/${id}`;
+  console.log("Embed URL:", embedUrl);
+
 
   return (
-    <section aria-labelledby="location-title">
-      <h2 id="location-title">Lieu</h2>
-      <p>{location.address}</p>
-      {/* Ici tu peux ajouter une map, iframe ou autre */}
+    <section className={s.location}>
+      <div className={s.location__title}>LOCALISATION</div>
+
+      <div className={s.location__map}>
+        <iframe
+          title="Carte interactive du festival"
+          src={embedUrl}
+          width="100%"
+          height="400"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
     </section>
   );
 }
