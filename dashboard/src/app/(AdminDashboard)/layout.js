af148f1a2553fb2b8b3c233a620e2001fd0610a8
HugoLcrf <lecerf.hugo04@gmail.com>
2025-06-11 22:41:57 +0200

    SidebarUp
    Ajout de metadata et correction hauteur

--- dashboard/src/app/(AdminDashboard)/layout.js
+++ dashboard/src/app/(AdminDashboard)/layout.js
@@ -1,6 +1,14 @@
 import "../../../../dashboard/src/app/globals.css";
 import SidebarAdmin from "@/components/AdminDashboard/SidebarAdmin/SidebarAdmin";
 
+export const metadata = {
+  title: "AdminDashboard - Festivable",
+  description: "Interface d’administration de Festivable permettant de gérer les festival. Un outil conçu pour faciliter la supervision et le bon fonctionnement de la plateforme.",
+  icons: {
+    icon: "/favicon.png", 
+  },
+};
+
 export default function Layout({ children }) {
   return (
     <div className="layout-wrapper">
--- dashboard/src/app/(OrganizerDashboard)/layout.js
+++ dashboard/src/app/(OrganizerDashboard)/layout.js
@@ -1,6 +1,15 @@
 import "../../../../dashboard/src/app/globals.css";
 import Sidebar from "@/components/OrganizerDashboard/Sidebar/Sidebar";
 
+
+export const metadata = {
+  title: "Dashboard - Festivable",
+  description: "Accédez à votre tableau de bord Festivable pour gérer facilement les informations de votre festival, la galerie photo, la carte interactive et les options d’accessibilité. Un espace dédié aux organisateurs pour valoriser l’inclusion et la visibilité de leur événement.",
+    icons: {
+    icon: "/favicon.png", 
+  },
+};
+
 export default function Layout({ children }) {
   return (
     <div className="layout-wrapper">
